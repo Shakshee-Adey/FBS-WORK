406c21ca4eb2e85d6dc2927a283f620ff08c080e
Shakshee-Adey <shaksheeadey@gmail.com>
2025-07-30 15:34:28 +0530

    Add files via upload

---
+++ c programming/Assignment0/Add1.c
@@ -0,0 +1,6 @@
+void main(){
+	int A=20;
+	int B=30;
+	int C = A+B;
+	printf("Addition is %d",C);
+}
---
+++ c programming/Assignment0/Area2.c
@@ -0,0 +1,5 @@
+void main(){
+	float Area,r=4;
+	Area = 3.14*r*r;
+	printf("Area of circle is %f",Area);
+}
