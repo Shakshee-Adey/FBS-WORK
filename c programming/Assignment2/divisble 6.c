cfe106c2ef67309ded1dac10c66d0a0f90bdf8d4
Shakshee-Adey <shaksheeadey@gmail.com>
2025-09-07 15:23:43 +0530

    Update divisble 6.c

--- c programming/Assignment2/divisble 6.c
+++ c programming/Assignment2/divisble 6.c
@@ -3,12 +3,12 @@ void main(){
 	int s=28;
 	if(s%3==0 && s%5==0)
 		printf("divisible by both");
-	else if(s%5==0 && s%3!=0)
-	    printf("divisble by 5 but not 3");
+	else if(s%5==0)
+	    printf("divisble by 5 ");
 	     else
-	       if(s%3==0 && s%5!=0)
-	         printf("divisble by 3 but not 5");
+	       if(s%3==0)
+	         printf("divisble by 3");
 	       else
-	         if(s%3!=0 && s%5!=0)
-	           printf("divisble by none");
+	         printf("divisble by none");
 }
+
