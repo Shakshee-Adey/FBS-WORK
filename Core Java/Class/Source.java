ed12beb1a40b77ae41c248b2cc88f86358bb2588
Shakshee-Adey <shaksheeadey@gmail.com>
2025-11-07 15:43:44 +0530

    Add files via upload

---
+++ Core Java/Class/Source.java
@@ -0,0 +1,12 @@
+class A{
+
+	public static void main(String[] args){
+		System.out.println("Hello World !!");
+	}
+}
+
+class B{
+          public static void main(String[] args){
+		System.out.println("Hello shakshee !!");
+	}
+}
