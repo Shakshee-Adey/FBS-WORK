03a3496d67aea43a1d6a2295d9b37ce4ffa6622c
Shakshee-Adey <shaksheeadey@gmail.com>
2025-11-12 11:55:38 +0530

    Add files via upload

---
+++ Core Java/Inheritance/TestDevice.java
@@ -0,0 +1,210 @@
+class Device {
+	String brand;
+    String model;
+    double price;
+    static int count = 0;
+
+    // Default constructor
+    Device() {
+        System.out.println("Device default constructor");
+        brand = "Unknown";
+        model = "Dont know";
+        price = 0;
+    }
+
+    // Parameterized constructor
+    Device(String brand, String model, double price) {
+        this.brand = brand;
+        this.model = model;
+        this.price = price;
+    }
+
+    String getBrand() {
+		return brand;
+	}
+
+	void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+	String getModel() {
+		return model;
+	}
+
+	void setModel(String model) {
+		this.model = model;
+	}
+
+	double getPrice() {
+		return price;
+	}
+
+	void setPrice(double price) {
+		this.price = price;
+	}
+        static int getCount() {
+		 return count;
+	   }
+        static void setCount(int count) {
+		   Device.count = count;
+	   }
+
+	void display() {
+        System.out.println("Brand is : " + brand);
+        System.out.println("Model is : " + model);
+        System.out.println("Price is : " + price);
+    }
+} // Device class end here
+class Laptop extends Device
+{
+	int ramSize;       
+    int storageSize;
+    
+ // Default constructor
+    Laptop() {
+    	super();
+        System.out.println("Device default constructor");
+        ramSize=32;       
+        storageSize=128;
+    }
+
+    // Parameterized constructor
+    Laptop(String brand, String model, double price,int ramSize,int storageSize) {
+    	super(brand,model,price);
+        System.out.println("Device para. constructor");
+        this.ramSize = ramSize;
+        this.storageSize = storageSize;
+    }
+
+	int getRamSize() {
+		return ramSize;
+	}
+
+	void setRamSize(int ramSize) {
+		this.ramSize = ramSize;
+	}
+
+	int getStorageSize() {
+		return storageSize;
+	}
+
+	void setStorageSize(int storageSize) {
+		this.storageSize = storageSize;
+	}
+	void display() {
+		super.display();
+        System.out.println("ramSize is : " + ramSize);
+        System.out.println("storageSize is : " + storageSize);
+	}
+}// Laptop class end here
+class Mobile extends Device
+{
+	int cameraM;
+    int batteryCapacity;
+   
+    // Default constructor
+    Mobile() {
+    	super();
+        System.out.println("Device default constructor");
+        cameraM=36;       
+        batteryCapacity=100;
+    }
+
+    // Parameterized constructor
+    Mobile(String brand, String model, double price,int cameraM,int batteryCapacity) {
+    	super(brand,model,price);
+        System.out.println("Device para. constructor");
+        this.cameraM = cameraM;
+        this.batteryCapacity = batteryCapacity;
+    }
+
+	int getCameraM() {
+		return cameraM;
+	}
+
+	void setCameraM(int cameraM) {
+		this.cameraM = cameraM;
+	}
+
+	int getBatteryCapacity() {
+		return batteryCapacity;
+	}
+
+    void setBatteryCapacity(int batteryCapacity) {
+		this.batteryCapacity = batteryCapacity;
+	}
+    void display() {
+    	super.display();
+        System.out.println("CameraM is : " + cameraM);
+        System.out.println("batteryCapacity is : " + batteryCapacity);
+	}
+ }//Mobile class end here
+class SmartWatch extends Device
+{
+	String displayType;
+	int batteryCapacity;
+	
+	// Default constructor
+    SmartWatch() {
+    	super();
+        System.out.println("Device default constructor");
+        displayType="LCD";       
+        batteryCapacity=100;
+    }
+
+    // Parameterized constructor
+    SmartWatch(String brand, String model, double price,String displayType,int batteryCapacity) {
+    	super(brand,model,price);
+        System.out.println("Device para. constructor");
+        this.displayType = displayType;
+        this.batteryCapacity = batteryCapacity;
+    }
+
+	String getDisplayType() {
+		return displayType;
+	}
+
+	void setDisplayType(String displayType) {
+		this.displayType = displayType;
+	}
+
+	int getBatteryCapacity() {
+		return batteryCapacity;
+	}
+
+	void setBatteryCapacity(int batteryCapacity) {
+		this.batteryCapacity = batteryCapacity;
+	}
+	void display() {
+		super.display();
+        System.out.println("displayType is : " + displayType);
+        System.out.println("batteryCapacity is : " + batteryCapacity);
+	}
+}// SmartWatch end here
+class TestDevice
+{
+   public static void main(String[] args) {
+
+		Laptop l1 = new Laptop();
+		l1.display();
+
+		Laptop l2 = new Laptop("HP", "Pavilion", 65000, 16, 512);
+		l2.display();
+		System.out.println("\n");
+
+		Mobile m1 = new Mobile();
+		m1.display();
+
+		Mobile m2 = new Mobile("Samsung", "Galaxy S24", 85000, 108, 5000);
+		m2.display();
+		System.out.println("\n");
+		
+		SmartWatch s1 = new SmartWatch();
+		s1.display();
+		
+		SmartWatch s2 = new SmartWatch("Apple", "Watch", 45000, "AMOLED", 18);
+		s2.display();
+
+		System.out.println("\nTotal Device: " + Device.getCount());
+	}
+}
