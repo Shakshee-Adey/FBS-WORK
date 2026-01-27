661d96519f06049ddefad7f7c0452016934ab261
Shakshee-Adey <shaksheeadey@gmail.com>
2026-01-27 20:13:14 +0530

    Add files via upload

---
+++ Core Java/Basicq/First.java
@@ -0,0 +1,19 @@
+
+//  Java string length without using length() method
+
+package sbasic;
+
+public class First {
+	
+	public static void main(String args[])
+	{
+		String str = "Shakshee";
+		int count=0;
+		for(int i=0; i<str.length(); i++)
+		{
+			count++;
+		}
+		System.out.println(count);
+	}
+	
+}
