9f061cb3bb21fc9a47f0530df62b99a3a9354ac6
Shakshee-Adey <shaksheeadey@gmail.com>
2026-02-16 13:30:14 +0530

    Add files via upload

---
+++ Core Java/MultiThreading/Test.java
@@ -0,0 +1,16 @@
+package multiThreading1;
+
+public class Test {
+
+	
+	public static void main(String args[])
+	{
+		String alfstr="abcdefghijklmnopqrtihjdlajndvvhjnjf";
+		
+		for(int i=0; i<alfstr.length(); i++)
+		{
+			System.out.println(alfstr.charAt(i));
+			
+		}
+	}
+}
